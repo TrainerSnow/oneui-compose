007f81373aecca2078005016444f94eadd7d3d70
simon <simon.bela.bumiller@gmx.de>
2023-07-20 14:39:55 +0200

    Add bnb to nav example screen

--- app/src/main/java/org/oneui/compose/ui/NavScreen.kt
+++ app/src/main/java/org/oneui/compose/ui/NavScreen.kt
@@ -2,10 +2,15 @@ package org.oneui.compose.ui
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Create
+import androidx.compose.material.icons.outlined.LocationOn
 import androidx.compose.material.icons.outlined.Menu
+import androidx.compose.material.icons.outlined.Share
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,9 +19,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import org.oneui.compose.base.Icon
 import org.oneui.compose.base.IconView
 import org.oneui.compose.layout.toolbar.CollapsingToolbarLayout
+import org.oneui.compose.navigation.BottomNavigationBar
+import org.oneui.compose.navigation.BottomNavigationBarItem
 import org.oneui.compose.navigation.CustomTabItem
 import org.oneui.compose.navigation.SubTabItem
 import org.oneui.compose.navigation.TabItem
@@ -118,6 +126,45 @@ fun NavScreen(
                                 text = it,
                                 selected = it == selectedSubTab,
                                 onClick = { selectedSubTab = it }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+
+        val navItems = (1..4).map { "Item $it" }
+        val icons = with(Icons.Outlined) { listOf(Share, AccountCircle, Create, LocationOn) }
+        var selectedNavItem by remember {
+            mutableStateOf(navItems.first())
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.TopCenter
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                TextSeparator(text = "Bottom Navigation Bar")
+                RoundedCornerBox(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    padding = PaddingValues(4.dp)
+                ) {
+                    BottomNavigationBar(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        navItems.forEach {
+                            BottomNavigationBarItem(
+                                modifier = Modifier
+                                    .weight(1F),
+                                label = it,
+                                onClick = { selectedNavItem = it },
+                                icon = Icon.Vector(icons[navItems.indexOf(it)])
                             )
                         }
                     }
