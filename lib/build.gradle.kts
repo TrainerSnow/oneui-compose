b0a0c96235bec2291c2aee43619be93dfc822053
simon <simon.bela.bumiller@gmx.de>
2023-08-10 17:36:41 +0200

    Release 0.1.1-h1f

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -74,7 +74,7 @@ afterEvaluate {
             create<MavenPublication>("maven") {
                 groupId = "com.github.TrainerSnow"
                 artifactId = "oneui-compose"
-                version = "0.1.0"
+                version = "0.1.1-hf1"
 
                 artifact(tasks.getByName("bundleReleaseAar"))
             }
