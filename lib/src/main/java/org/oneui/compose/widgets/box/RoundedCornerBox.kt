b06ba1b1b4e74bb5558ac6da3146c2f0330f39e8
simon <simon.bela.bumiller@gmx.de>
2023-08-13 08:18:50 +0200

    Add list rounded corner box

--- lib/src/main/java/org/oneui/compose/widgets/box/RoundedCornerBox.kt
+++ lib/src/main/java/org/oneui/compose/widgets/box/RoundedCornerBox.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ripple.rememberRipple
@@ -14,12 +16,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import org.oneui.compose.theme.OneUITheme
 import org.oneui.compose.theme.locals.ProvideBackgroundColor
+import org.oneui.compose.util.ListPosition
 
 /**
  * Contains default values to define a [RoundedCornerBox]
@@ -86,6 +92,77 @@ fun RoundedCornerBox(
             content()
         }
     }
+}
+
+
+/**
+ * Composable for a oneui-style box with rounded corners, that adjusts its' shape and border to the position of a list that its in.
+ *
+ * @param modifier The modifier to be applied to the container
+ * @param colors The [RoundedCornerBoxColors] to use
+ * @param onClick The callback to invoke when clicked.
+ * @param interactionSource The [MutableInteractionSource] to use
+ * @param contentAlignment The [Alignment] to align the content by
+ * @param padding The padding to use
+ * @param listPosition The [ListPosition] determining the shape and border
+ * @param content The content of the box
+ */
+@Composable
+fun RoundedCornerListItem(
+    modifier: Modifier = Modifier,
+    colors: RoundedCornerBoxColors = roundedCornerBoxColors(),
+    onClick: (() -> Unit)? = null,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    contentAlignment: Alignment = Alignment.Center,
+    padding: PaddingValues = RoundedCornerBoxDefaults.padding,
+    listPosition: ListPosition = ListPosition.Middle,
+    content: @Composable BoxScope.() -> Unit
+) {
+    val ld = LocalLayoutDirection.current
+    val strokeColor = OneUITheme.colors.seslListDividerColor
+    RoundedCornerBox (
+        modifier = modifier
+            .drawWithContent {
+                drawContent()
+                if (listPosition == ListPosition.Middle || listPosition == ListPosition.First) {
+                    drawLine(
+                        color = strokeColor,
+                        start = Offset(
+                            x = padding.calculateStartPadding(ld).toPx(),
+                            y = size.height
+                        ),
+                        end = Offset(
+                            x = size.width - padding.calculateEndPadding(ld).toPx(),
+                            y = size.height
+                        ),
+                        strokeWidth = 0.4F
+                    )
+                }
+            },
+        colors = colors,
+        onClick = onClick,
+        shape = roundedCornerShape(
+            dp = 26.dp,
+            listPosition = listPosition
+        ),
+        interactionSource = interactionSource,
+        contentAlignment = contentAlignment,
+        padding = padding,
+        content = content
+    )
+}
+
+private fun roundedCornerShape(dp: Dp = 26.dp, listPosition: ListPosition): Shape = when (listPosition) {
+    ListPosition.First -> RoundedCornerShape(
+        topStart = dp, topEnd = dp
+    )
+    ListPosition.Middle -> RoundedCornerShape(size = 0.dp)
+    ListPosition.Last -> RoundedCornerShape(
+        bottomStart = dp, bottomEnd = dp
+    )
+    ListPosition.Single -> RoundedCornerShape(
+        dp
+    )
 }
 
 /**
