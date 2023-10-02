7f21e1eae5a908343054ce4fddb162632d132539
simon <simon.bela.bumiller@gmx.de>
2023-10-02 09:56:29 +0200

    composeable for the navigation rail

---
+++ lib/src/main/java/org/oneui/compose/layout/rail/NavigationRail.kt
@@ -0,0 +1,145 @@
+package org.oneui.compose.layout.rail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.oneui.compose.layout.internal.BaseNavigationRail
+import org.oneui.compose.layout.internal.DrawerState
+import org.oneui.compose.layout.internal.rememberSlidingDrawerState
+import org.oneui.compose.theme.OneUITheme
+import org.oneui.compose.theme.locals.ProvideBackgroundColor
+
+/**
+ * Composable for a oneui-style Navigation rail. This acts as an alternative to a Drawer or a Bottom Navigation Bar on larger screens.
+ *
+ * @param modifier The [Modifier] to apply to the container
+ * @param state The [DrawerState] to control the Rail
+ * @param colors The [NavRailColors] to apply
+ * @param shape The [Shape] of the rail
+ * @param layoutPadding The padding to apply to the entire layout
+ * @param railPadding The padding to apply to the rail
+ * @param railHeader The section of the rail which includes e.g. a settings icon
+ * @param railContent The section of the rail which includes the actual navigation destinations
+ * @param content The actual content to be put next to the rail.
+ */
+@Composable
+fun NavRailLayout(
+    modifier: Modifier = Modifier,
+    state: DrawerState = rememberSlidingDrawerState(),
+    colors: NavRailColors = navRailColors(),
+    shape: Shape = NavRailDefaults.shape,
+    layoutPadding: PaddingValues = NavRailDefaults.layoutPadding,
+    railPadding: PaddingValues = NavRailDefaults.railPadding,
+    railHeader: @Composable ColumnScope.() -> Unit,
+    railContent: @Composable ColumnScope.() -> Unit,
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .background(colors.background)
+            .padding(layoutPadding)
+    ) {
+        BaseNavigationRail(
+            modifier = Modifier
+                .fillMaxSize(),
+            state = state,
+            minWidth = NavRailDefaults.closedSize,
+            maxWidth = NavRailDefaults.openedSize,
+            railContent = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(shape)
+                        .background(colors.railBackground, shape)
+                        .padding(railPadding)
+                ) {
+                    ProvideBackgroundColor(colors.railBackground) {
+                        railHeader()
+                        railContent()
+                    }
+                }
+            }
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                ProvideBackgroundColor(colors.background) {
+                    content()
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Contains default values for a [NavRailLayout]
+ */
+object NavRailDefaults {
+
+    val layoutPadding = PaddingValues(
+        top = 16.dp
+    )
+
+    val railPadding = PaddingValues(
+        horizontal = 8.dp
+    )
+    
+    val closedSize = 70.dp
+
+    val openedSize = 350.dp
+
+    val shape = RoundedCornerShape(
+        topEnd = 15.dp,
+        bottomEnd = 15.dp
+    )
+
+}
+
+/**
+ * Contains the colors used for a [NavRailLayout]
+ */
+data class NavRailColors(
+
+    val background: Color,
+
+    val railBackground: Color
+
+)
+
+/**
+ * Constructs the default [NavRailColors]
+ *
+ * @param background The color to be used as a background
+ * @param railBackground The color to be used as a background for the actual rail
+ */
+@Composable
+fun navRailColors(
+    background: Color = OneUITheme.colors.seslRoundAndBgcolor,
+    railBackground: Color = OneUITheme.colors.navigationRailBackground
+) = NavRailColors(background, railBackground)
+
+
+@Preview
+@Composable
+private fun NavigationRailPreview() {
+    NavRailLayout(
+        modifier = Modifier.fillMaxSize(),
+        railHeader = { },
+        railContent = { },
+        content = { }
+    )
+}
